f4b4685e164afef38552a5b6157d39284de3ae96
fkworld <fkworld@foxmail.com>
2017-11-16 15:55:53 +0800

    修复代码BUG

--- 01bag.py
+++ 01bag.py
@@ -3,46 +3,27 @@
     阶段：在前N件物品中，选取若干件物品放入背包中
     状态：在前N件物品中，选取若干件物品放入所剩空间为W的背包中的所能获得的最大价值
     决策：第N件物品放或者不放
+
+    缺点：时间复杂度（number*weight）太高了！在实际使用中耗费的资源太大
 '''
 
 from numpy import zeros
 from random import randint
 
-
-class Item(object):
-    def __init__(self, id, weight, value):
-        self.id = id
-        self.weight = weight
-        self.value = value
-
-
-
-def f(items, number, weight_max):
-    o = 0
-    result = zeros((number+1, weight_max+1))  # 初始化结果二维数组
-    for i in range(1, number+1):
-        for j in range(1, weight_max+1):
-            o += 1
-            if weight_max < items[i].weight:
-                # 如果第i个物品
-                result[i][j] = result[i - 1][j]
+def solve(vlist,wlist,totalWeight,totalLength):
+    resArr = zeros((totalLength+1,totalWeight+1))
+    for i in range(1,totalLength+1):
+        for j in range(1,totalWeight+1):
+            if wlist[i] <= j:
+                resArr[i,j] = max(resArr[i-1,j-wlist[i]]+vlist[i],resArr[i-1,j])
             else:
-                v1 = result[i - 1][j]
-                v2 = result[i - 1][j - items[i].weight] + items[i].value
-                result[i][j] = max(v1, v2)
-    print(o)
-    print(result[number][weight_max])
-
-def init_items(number):
-    items = list()
-    for i in range(number):
-        random_number = randint(0,20)
-        print(random_number,end=' ')
-        item = Item(i,random_number, random_number)
-        items.append(item)
-    return items
-
+                resArr[i,j] = resArr[i-1,j]
+    return resArr[-1,-1] # 数组的负索引：list[-n] == list[len(list) - n]
 
 if __name__ == '__main__':
-    items = init_items(11)
-    f(items, 10, 100)
+    v = [0,60,100,120]
+    w = [0,60,100,120]
+    weight = 160
+    n = 3
+    result = solve(v,w,weight,n)
+    print(result)
