26cc58b3a447a996ac549f6abfc26b235eb5331b
fkworld <fkworld@foxmail.com>
2017-11-30 15:07:13 +0800

    继续完善node下载文件的功能

--- node.py
+++ node.py
@@ -1,11 +1,14 @@
 import requests
 
+
 class Node(object):
     def __init__(self):
         self.level = 1
         self.server_host = "localhost"
         self.server_port = 5000
-        self.requests_url = "http://" + self.server_host + ":" + str(self.server_port)
+        self.requests_url = "http://" + \
+            self.server_host + ":" + str(self.server_port)
+        self.current_task = {}              # 节点当前任务
 
     def self_test(self):
         # 节点的自我检测功能，检测当前状态是否合格
@@ -25,19 +28,36 @@ class Node(object):
     def download_task(self):
         # 节点向服务器请求下载任务资源
         # 由于文件比较大，所以采用stream=True的文件流下载方式
-        result = requests.get(self.requests_url + "/needtask", stream=True)
-        with open("download.md", "wb") as pdf:
+        result = requests.get(self.current_task['download_url'], stream=True)
+        filetype = result.headers['_filetype']
+        # 这种下载方法可能有问题，比如在网络不稳定的情况下如何进行断点续传问题，参考使用curl工具来实现对资源文件的下载
+        # 构建保存的文件名
+        try:
+            # 如果不存在download目录，则添加download目录
+            import os
+            if not os.path.exists('download'):
+                os.mkdir('download')
+        except expression as identifier:
+            pass
+        finally:
+            filename = "download/" + \
+                str(self.current_task['id']) + '_sourcefile' + filetype
+        with open(filename, "wb") as _sourcefile:
             for chunk in result.iter_content(chunk_size=1024):
                 if chunk:
-                    pdf.write(chunk)
+                    _sourcefile.write(chunk)
 
     def do_task(self):
         # 执行文件，这里还需要用一个redis做任务状态缓存
         pass
 
+
 def main():
     mynode = Node()
-    mynode.ask_task()
+    mynode.current_task = {
+        'id': 1, 'download_url': "http://localhost:5000/download_sourcefile/REDEME.md"}
+    mynode.download_task()
+
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -17,7 +17,7 @@ from form import file_images
 # 将 app 的 config 配置注册到 UploadSet 实例 file_images （这是人说的话吗）
 configure_uploads(app, file_images)
 # 限制上传文件的大小
-patch_request_class(app, 32 * 1024 * 1024) # 32MB
+patch_request_class(app, 32 * 1024 * 1024)  # 32MB
 
 
 server_db = ServerDB(app)
@@ -54,11 +54,13 @@ def manage_file():
     file_list = file.searh_all()
     return render_template('manage.html', file_list=file_list)
 
+
 @app.route('/register')
 def register():
     print("node is success register.")
     print(request)
     return "sucecess"
+
 
 @app.route('/ask_task')
 def ask_task():
@@ -66,15 +68,21 @@ def ask_task():
     # 包含：任务id，任务资源下载地址，任务结果上传地址，任务目标节点
     from TaskQueue import TaskQueue
     print("node is asking for task...")
-    test = TaskQueue(download_url="downlaod",target_node=1,upload_url="upload")
+    test = TaskQueue(download_url="downlaod",
+                     target_node=1, upload_url="upload")
     return jsonify(test.to_json_dict())
 
-@app.route('/needtask')
-def needtask():
-    print("node is asking for task.")
-    response = make_response(send_file("README.md"))
-    response.headers["Content-Disposition"] = "attachment; filename=README.md;"
+
+@app.route('/download_sourcefile/<sourcefile_filename>')
+def download_sourcefile(sourcefile_filename):
+    print("node is download sourcefile...")
+    filename = "README.md"
+    response = make_response(send_file(filename))
+    response.headers["Content-Disposition"] = "attachment"
+    # 这里添加一个filetype项目来标识文件的类型
+    response.headers['_filetype'] = '.' + filename.split('.')[-1]  # 用-1来获取最后一个元素
     return response
+
 
 @app.route('/3dmodel')
 def _3dmodel():
@@ -83,7 +91,8 @@ def _3dmodel():
         text = file.read()
     finally:
         file.close()
-    return render_template("3dmodel.html", file_js = text)
+    return render_template("3dmodel.html", file_js=text)
+
 
 if __name__ == '__main__':
     app.run()
