3a3e4d971dde0c48c8140a43a7f9d948c4f3a916
fkworld <fkworld@foxmail.com>
2017-11-30 14:15:08 +0800

    添加从taskqueue中取出一项任务的功能

--- TaskQueue.py
+++ TaskQueue.py
@@ -3,11 +3,14 @@ from server import server_db
 db = server_db.db
 
 
+'''
+任务队列实际上应该用redis等nosql数据库来做，这里稍微简化一下...
+'''
 class TaskQueue(db.Model):
     __tablename__ = 'task_queue'
     id = db.Column(db.Integer, primary_key=True)        # 任务id
     download_url = db.Column(db.Text)                   # 任务资源下载地址
-    target_node = db.Column(db.Integer)                 # 目标节点
+    target_node = db.Column(db.Integer,default=0)       # 目标节点
     upload_url = db.Column(db.Text)                     # 结果上传地址
 
     def __init__(self, download_url, target_node, upload_url):
@@ -15,9 +18,19 @@ class TaskQueue(db.Model):
         self.target_node = target_node
         self.upload_url = upload_url
 
-    def add_to_db(self):
+    def add(self):
         db.session.add(self)
         db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    # 获取队列中的第一项，然后在列表中删除这一项
+    def get(self):
+        self = self.query.first()
+        self.delete()
+        return self
 
     # 转换成字典供给flaskrequest转换成json
     def to_json_dict(self):
@@ -28,3 +41,5 @@ class TaskQueue(db.Model):
             "upload_url": self.upload_url
         }
         return task_json_dict
+
+
--- test.py
+++ test.py
@@ -1,8 +1,11 @@
+from server import app
 from TaskQueue import TaskQueue
 
 def main():
     test = TaskQueue(download_url="downlaod",target_node=1,upload_url="upload")
-    print(test.to_json())
+    with app.app_context():
+        test.add()
+        print(test.get())
 
 if __name__ == '__main__':
     main()
