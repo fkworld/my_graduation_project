dd24bee1d7ce9733f4f075bfb37ba760cfc05ef4
fkworld <fkworld@foxmail.com>
2018-05-10 17:37:50 +0800

    添加bootstrap4前端框架

--- Web.py
+++ Web.py
@@ -18,5 +18,5 @@ class Web(object):
         self.load_blueprint()
 
     def load_blueprint(self):
-        from web.view_server import view_server
+        from web_flask.view_server import view_server
         self.app.register_blueprint(view_server, url_prefix='')
