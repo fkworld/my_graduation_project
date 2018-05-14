4620c22dafb3d1b9d768cffedabb11fb6cea9232
fkworld <fkworld@foxmail.com>
2018-05-14 18:44:59 +0800

    完善模型预览和结果预览的前端显示框架

--- web_flask/view_server.py
+++ web_flask/view_server.py
@@ -15,6 +15,11 @@ def index():
     return flask.render_template("index.html")
 
 
+@view_server.route('/test')
+def test():
+    return flask.render_template('preview_result.html')
+
+
 @view_server.route('/node_manager')
 def node_manager():
     node_list = server_start.server.node_manager.show_all_node()
@@ -26,14 +31,32 @@ def task_manager():
     task_list = server_start.server.task_manager.show_all_task()
     return flask.render_template('task_manager.html', task_list=task_list)
 
+
 @view_server.route('/use_guide')
 def use_guide():
     return flask.render_template('use_guide.html')
+
 
 @view_server.route('/develop_document')
 def develop_document():
     return flask.render_template('develop_ducument.html')
 
+
 @view_server.route('/source_code')
 def source_code():
-    return flask.redirect("https://github.com/fkworld/my_graduation_project")
+    return flask.redirect('https://github.com/fkworld/my_graduation_project')
+
+
+@view_server.route('/preview_3dmodel')
+def preview_3dmodel():
+    '''预览3D模型
+    实际使用需要传入参数查询
+    '''
+    return flask.render_template('preview_3dmodel.html')
+
+@view_server.route('/preview_result')
+def preview_result():
+    '''预览渲染结果
+    实际使用需要传入参数查询
+    '''
+    return flask.render_template('preview_result.html')
