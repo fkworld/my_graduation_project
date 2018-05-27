bda0a466fdbaeb39c21ab9ef49462a6c9f916117
fkworld <fkworld@foxmail.com>
2018-05-27 16:21:33 +0800

    修改代码结构

--- node_start.py
+++ node_start.py
@@ -1,6 +1,23 @@
+from socketIO_client import SocketIO, BaseNamespace
 
-from socketIO_client import SocketIO, LoggingNamespace
 
-with SocketIO('127.0.0.1', 5000) as client:
-    client.emit('message', 'hello world.')
-    # client.emit('error', 'data')
+class Test(BaseNamespace):
+    def on_connect(self):
+        print('[Connected]')
+
+    def on_reconnect(self):
+        print('[Reconnected]')
+
+    def on_disconnect(self):
+        print('[Disconnected]')
+
+
+def main():
+    with SocketIO('127.0.0.1', 5000, Test, transports='websocket') as client:
+        client.emit('message', 'hello world.')
+        client.emit('message', 'hello world-2.')
+        client.wait(10)
+
+
+if __name__ == '__main__':
+    main()
