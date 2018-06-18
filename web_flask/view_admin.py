53ef4d4d47952787bbbedcadd84afa65d45582a3
fengyong <fkworld@foxmail.com>
2018-06-18 23:52:55 +0800

    添加node数据库的初始化

--- web_flask/view_admin.py
+++ web_flask/view_admin.py
@@ -3,8 +3,23 @@
 
 from flask import Blueprint, render_template, url_for, request, redirect, session
 
+import TaskManager
+import NodeManager
+
 view_admin = Blueprint('view_admin', __name__, template_folder='templates')
+
+TM = TaskManager.TaskManager()
+NM = NodeManager.NodeManager()
+
 
 @view_admin.route('/')
 def index():
     return render_template('admin.html')
+
+
+@view_admin.route('/init_database')
+def init_database():
+    TM.init_db()
+    NM.init_db()
+    return redirect(url_for("view_admin.index"))
+
