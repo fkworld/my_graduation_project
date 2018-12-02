0247a46401a9ff90a06854cb1f0cbbf54d678606
fengyong <fkworld@foxmail.com>
2018-12-03 00:55:00 +0800

    添加一个node节点的伪数据

--- node_start.py
+++ node_start.py
@@ -1,9 +1,11 @@
 from socketIO_client import SocketIO, BaseNamespace
 from NodeSourceManager import NodeSourceManager
+import faker
 
 
+is_down = True
 
-is_down= True
+
 class Test(BaseNamespace):
     def on_connect(self):
         print('[Connected]')
@@ -37,7 +39,25 @@ def main():
     with SocketIO('127.0.0.1', 5000, Test, transports='websocket') as client:
         client.emit('GET_TASK')
         client.wait(5)
- 
+
+# fengyong-fake-data
+# websocket模式无法持续获取任务，这里采用一个伪数据
+
+
+def fake_main():
+    for i in range(10):
+        print("on GET_TASK response:json=", fake_json(i))
+
+# fengyong-fake-data
+# 形成一个伪json数据
+def fake_json(i):
+    md5 = faker.Faker(locals="zh_CN").md5()
+    return {
+        'id': i,
+        'passcode': md5,
+        'sourcefile_path': md5+".blender"
+    }
+
 
 if __name__ == '__main__':
-    main()
+    fake_main()
